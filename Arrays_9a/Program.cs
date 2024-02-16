9ffa580f0cec42cfc1a5f846374b22392160af87
SianaStoicheva <ss12712978@edu1.pgzaimov.com>
2024-02-16 12:28:46 +0200

    Vtora proba ot ZAIMOV

---
+++ Arrays_9a/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Arrays_InputOtput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklariram masiv ot 13 elementa
+            double[] grades = new double[13];//{1,2,3,4}
+            string[] names = new string[13]{"Lubo", "Milen","Misho","Rado","Svetlio","Siana","Silviq","Stanislav","Tedo","Hristian","Hristomir","Cvetelina","Cvetina"};
+            //Vuvejda se elementite ot klaviatyrta
+            for (int index = 0; index < grades.Length; index++)
+            {
+                Console.Write($"{index+14}. {names[index]} e >>>");
+                grades[index] = double.Parse(Console.ReadLine());
+            }
+            //Obrabotka
+            grades[9] = 6;
+            names[8] = "Teodor";
+            grades[1] = 5.234;
+            grades[1] = Math.Ceiling(5.234);
+            Console.WriteLine($"Na {names[1]} is grade is {grades[1]}");
+            grades[6] = 6;
+            //Otpechatvane na rezultat
+            for (int j = 0;j < names.Length;j++) 
+            {
+                Console.WriteLine($"{j + 14}.{names[j]}");
+            }
+        }
+    }
+}
---
+++ zad2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Day broi na elementite: ");
+            int n = int.Parse(Console.ReadLine());
+            //array
+            int[] numbers = new int[n];
+            //1.input the elements
+            for (int i = 0; i < numbers.Length-1; i++) 
+            {
+                Console.Write($"num[{i}] = ");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            //2.
+            long sum = 0l;
+            for(int i = 0; i < numbers.Length - 1; i++)
+            {
+                sum = sum + numbers[i];
+            }
+            Console.WriteLine($"Sum = {sum}");
+
+            //4.
+            int min = int.MaxValue;
+            int minpOS = int.MaxValue;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] < min )
+                {
+                    min = numbers[i];
+                    minpOS = i;
+                }
+            }
+            Console.WriteLine($"{min} ===> {minpOS} index");
+
+            //5.Print ALL element
+            for (int i = 0;i < numbers.Length-1; i++)
+            {
+                Console.WriteLine($"num[{i}] = {numbers[i]}");
+            }
+        }
+    }
+}
